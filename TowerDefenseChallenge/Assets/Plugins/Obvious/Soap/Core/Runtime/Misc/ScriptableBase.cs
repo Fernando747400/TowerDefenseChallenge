853370ea41383c2ea6ab9d3dff203faef3b524b5
Azul24 <azul24@amerike.edu.mx>
2024-02-15 18:32:19 -0600

    Initial Assets

---
+++ TowerDefenseChallenge/Assets/Plugins/Obvious/Soap/Core/Runtime/Misc/ScriptableBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Obvious.Soap
+{
+    /// <summary>
+    /// Base classes of all ScriptableObjects in Soap
+    /// </summary>
+    [Serializable]
+    public abstract class ScriptableBase : ScriptableObject
+    {
+        public virtual void Reset() { }
+        public Action RepaintRequest;
+        [HideInInspector]
+        public int CategoryIndex = 0;
+        public abstract Type GetGenericType { get; }
+    }
+}
---
+++ TowerDefenseChallenge/Assets/Plugins/Obvious/Soap/Core/Runtime/ScriptableEvents/ScriptableEventBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Obvious.Soap
+{
+    [System.Serializable]
+    public abstract class ScriptableEventBase : ScriptableBase
+    {
+        [Tooltip("Enable console logs when this event is raised.")]
+        [SerializeField] 
+        protected bool _debugLogEnabled = false;
+        public bool DebugLogEnabled => _debugLogEnabled;
+    }
+}
