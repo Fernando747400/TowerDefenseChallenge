41f73b6b8f25d104115f27f8156ec675e9e334a5
Fernando747 <fernandojcr5@gmail.com>
2024-02-21 01:45:03 -0600

    New input system

---
+++ TowerDefenseChallenge/Assets/F747/_Code/Towers/TowerSocket.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSocket : MonoBehaviour
+{
+    private bool _isActive;
+    private bool _hasTower;
+    private GameObject _towersGroup;
+}
